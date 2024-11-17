4e284520b5b4f02802d9a3021487f8fe441511b1
Anubhav Sen <anubhav.sen@code.berlin>
2024-11-17 09:27:22 +0100

    refactored the user model to allow for better data validation

--- authentication-service/app/models.py
+++ authentication-service/app/models.py
@@ -6,13 +6,25 @@ from pydantic import EmailStr
 from sqlalchemy import String
 from sqlmodel import Field, SQLModel, select
 
-class User(SQLModel, table=True):
+class UserBase(SQLModel):
+    username: str = Field(max_length=50)
+    email_address: EmailStr = Field(max_length=255, sa_type=String(), unique=True, nullable= False)
+
+class User(UserBase, table=True):
     __tablename__ = "users"
 
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    username: str = Field(max_length=50)
-    email_address: EmailStr = Field(max_length=255, sa_type=String(), unique=True, nullable= False)
     password: str = Field(max_length=255)
     date_created: datetime = Field(default_factory=datetime.utcnow, nullable=False)
     date_updated: datetime = Field(default_factory=datetime.utcnow, nullable=False)
 
+class UserPublic(UserBase):
+    id: uuid.UUID
+
+class UserCreate(UserBase):
+    password: str = Field(max_length=255)
+
+class UserUpdate():
+    username: str | None = None
+    password: str | None = None 
+    
